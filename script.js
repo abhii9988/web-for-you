378b8f2c9bd9ef6c9d014ebb00b30049350bf35b
abhii9988 <151422503+abhii9988@users.noreply.github.com>
2024-02-09 17:40:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function handleYes() {
+  window.location.href = "yes.html";
+}
+
+function handleNo() {
+  window.location.href = "no.html";
+}
+
+function handleForgiveYes() {
+  window.location.href = "thankyou.html";
+}
+
+function handleForgiveNo() {
+  window.location.href = "no.html";
+}
+
+function goBack() {
+  window.history.back();
+}
+function handleForgiveNah() {
+  window.location.href= "nah.html"
+}
